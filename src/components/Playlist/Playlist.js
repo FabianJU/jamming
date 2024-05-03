c0dc08acecbff5f005eaf8d0916e3878c04eb3df
Fabian <jujescufabian@gmail.com>
2024-05-03 13:41:05 -0400

    first commit

---
+++ src/components/Playlist/Playlist.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import TrackList from '../TrackList/TrackList';
+import './Playlist.css';
+
+function Playlist(props) {
+
+    const handleNameChange = (event) => {
+        props.onNameChange(event.target.value);
+    }
+
+    return (
+        <div className="Playlist">
+            <input defaultValue={props.playlistName} onChange={handleNameChange}/>
+            <TrackList tracks={props.playlistTracks} 
+                       onRemove={props.onRemove} 
+                       isRemoval={true}/>
+            <button className="Playlist-save" onClick={props.onSave}>SAVE TO SPOTIFY</button>
+        </div>
+    )
+}
+
+export default Playlist;
---
+++ src/components/TrackList/TrackList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './TrackList.css';
+import Track from '../Track/Track';
+
+function TrackList(props) {
+    return (
+        <div className="TrackList">
+            {props.tracks.map(track => {
+                return <Track key={track.id} 
+                              track={track} 
+                              onAdd={props.onAdd} 
+                              onRemove={props.onRemove} 
+                              isRemoval={props.isRemoval}/>
+            })}
+        </div>
+    )
+}
+
+export default TrackList;
